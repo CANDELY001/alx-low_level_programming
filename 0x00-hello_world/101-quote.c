7f292c381e1d11c80c2de8bb73982a1e3bee488d
charifa <candely1charifa@gmail.com>
2023-03-16 12:04:26 -0700

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+# include<unistd.h>
+/**
+ * main - A program that prints the lines with write function
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+char * message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, message, 59);
+return (1);
+}
