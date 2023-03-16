428340e520f4a20cc2157de2bc12cc4e9f785dde
charifa <candely1charifa@gmail.com>
2023-03-16 12:39:52 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,6 @@
 */
 int main(void)
 {
-char *message = "and that piece of art is useful\"";
-*message += " - Dora Korpar, 2015-10-19\n";
-write(1, message, 47);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 47);
 return (1);
 }
