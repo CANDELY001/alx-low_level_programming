a45a956eb0aec74f0f23f2a776e1b1b834cc2cc0
charifa <candely1charifa@gmail.com>
2023-03-17 11:24:23 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase,
+ * followed by a new line.
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a == 'q' || a == 'e')
+		{
+			continue;
+		}
+		putchar(a);
+		a = a + 1;
+	}
+	putchar('\n');
+	return (0);
+}
