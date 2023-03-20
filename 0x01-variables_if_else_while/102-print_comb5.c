fc29d067b9c7aa558e5473f970cd439189e91f14
charifa <candely1charifa@gmail.com>
2023-03-20 10:23:44 -0700

    task12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - A program that prints all possible combinations
+ * of two two-digit numbers.
+ * Return: 0 (Sucess)
+*/
+int main(void)
+{
+	int a = '0', b;
+
+	for (; a <= '98'; a++)
+	{
+		for (b = a + 1; b <= '99'; b++)
+		{
+			if (a != '98' || b != '99')
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+			}
+			putchar(', ');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
