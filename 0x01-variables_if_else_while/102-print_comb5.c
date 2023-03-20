d180bdbd78a7435b32c0e8c44ebb3aff653edaec
charifa <candely1charifa@gmail.com>
2023-03-20 16:21:04 -0700

    task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,14 +12,15 @@ int main(void)
 	{
 		for (b = a + 1; b <= 99; b++)
 		{
-			if (a != 98 && b != 99)
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 98 && b == 99)
 			{
-				putchar((a / 10) + '0');
-				putchar((a % 10) + '0');
-				putchar(' ');
-				putchar((b / 10) + '0');
-				putchar((b % 10) + '0');
-			}
+				continue;
+			} 
 			putchar(',');
 			putchar(' ');
 		}
