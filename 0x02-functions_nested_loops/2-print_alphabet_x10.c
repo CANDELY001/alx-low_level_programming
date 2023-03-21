204c4c913af1ea5a94259b2b19530acec1bdbbf9
charifa <candely1charifa@gmail.com>
2023-03-21 10:18:06 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+*/
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i = 1;
+
+	while (i <= 10)
+	{
+		for (; c <= 'z'; c++)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
