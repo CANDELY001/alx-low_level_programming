34898cf811441a96cd480e9d7789db4638ac08ed
charifa <candely1charifa@gmail.com>
2023-03-21 11:38:45 -0700

    task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - a function that prints the sign of a number.
+ * @n: is a parameter
+ * Return:1 (result)
+*/
+int print_sign(int n)
+{
+	if (n > '0')
+	{
+		_putchar('+');
+		return (1);
+	}
+       	if (n == '0')
+	{
+		_putchar(n);
+		return (0);
+	}
+	if (n < '0')
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
