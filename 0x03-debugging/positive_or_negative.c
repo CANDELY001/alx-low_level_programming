2fa69f95b29988477b5caea5f9cdef4d241101d2
charifa <candely1charifa@gmail.com>
2023-03-23 06:11:06 -0700

    task0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
- * main - A program that  will assign a random number to
- * the variable n each time it is executed.
- * Complete the source code in order to print whether
- * the number stored in the variable n is positive or negative.
+ * positive_or_negative - A function
+ * @n: parameter
  * Return: 0 (Success)
  */
 int postitive_or_negative(int n)
