4f1cc8b6b3f7edc5e972c5449985b1eb0716c706
charifa <candely1charifa@gmail.com>
2023-03-23 08:18:55 -0700

    task0

---
+++ 0x03-debugging/postitive_or_negative.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * postitive_or_negative - A function
+ * @n: parameter
+ * Return: void return
+ */
+void postitive_or_negative(int n)
+{
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	} else
+	{
+		printf("%d is negative\n", n);
+	}
+}
