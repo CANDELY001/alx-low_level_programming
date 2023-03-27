7712b0cdede0f704e0ebf78c3fcc9138e3e7b598
charifa <candely1charifa@gmail.com>
2023-03-27 15:16:39 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - a function that prints a string, followed by a new line, to stdout
+ * @str: a parameter
+*/
+void _puts(char *str)
+{
+	int i = 0;
+
+	for(; str[i] != '\0'; i++)
+	{
+		puts(str[i]);
+	}
+}
