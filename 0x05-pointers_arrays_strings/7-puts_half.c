4040db11f01bf15ac7366be25523ebe45b3bbcd0
charifa <candely1charifa@gmail.com>
2023-03-27 18:36:56 -0700

    task6

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,17 +14,17 @@ void puts_half(char *str)
 		len++;
 	}
 
-	if (len % 2)
+	if (len % 2 == 0)
 	{
 		i = len / 2;
 		for (; i < len; i++)
 		{
 			_putchar(str[i]);
 		}
-	} else if
+	} else if (len % 2)
 	{
 		i = (len - 1) / 2;
-		for (; i < len; i++)
+		for (; i < len - 1; i++)
 		{
 			_putchar(str[i]);
 		}
