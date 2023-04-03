cf1919f26c6f7a9959e6319e7d4dd86a4c16db62
charifa <candely1charifa@gmail.com>
2023-04-03 16:29:12 -0700

    task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,11 +8,17 @@
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
+	{
 		if (*s == c)
-			return ((char *)s);
+		{
+			return (s);
+		}
 	        s++;
+	}
 	if (c == '\0')
-		return ((char *)s);
+	{
+		return (s);
+	}
 
 	return NULL;
 }
