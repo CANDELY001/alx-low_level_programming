bfce18fed61c0cbbe31f1728487b4b23c0c63789
charifa <candely1charifa@gmail.com>
2023-04-03 16:02:20 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memset - function that fills memory with a constant byte
+ * @s: parameter
+ * @b: parameter
+ * @n: parameter
+ * Return: result
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+	{
+		*ptr = (unsigned char) b;
+		ptr ++;
+	}
+	return (s);
+}
