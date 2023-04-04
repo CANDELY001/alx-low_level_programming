37a15a3c6f131225e448b2bb7cf89c78d27d7605
charifa <candely1charifa@gmail.com>
2023-04-03 17:19:55 -0700

    task4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,15 +8,16 @@
 char *_strpbrk(char *s, char *accept)
 {
 	char *p1 = s;
-	char *p2 = accept;
+	char *p2;
 
 	while (*p1 != '\0')
 	{
+		p2 = accept;
 		while (*p2 != '\0')
 		{
 			if (*p1 == *p2)
 			{
-				return (s);
+				return (p1);
 			}
 			p2++;
 		}
