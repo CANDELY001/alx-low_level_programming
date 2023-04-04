58b2bcbcdcf325887f677e8dfb59bf82723e4faf
charifa <candely1charifa@gmail.com>
2023-04-03 18:04:15 -0700

    task6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,18 +7,17 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	char *p = a;
-	int row_size = sizeof(p[0]) / sizeof(p[0][0]);
+	/*int row_size = sizeof(a[0]) / sizeof(a[0][0]);*/
 	int i, j;
 
-	for (i = 0; i < row_size; i++)
+	for (i = 0; i < 8; i++)
 	{
 		j = 0;
-		while (p[i][j] != '\0')
+		while (a[i][j] != '\0')
 		{
-			printf("%d", p[i][j]);
+			_putchar(a[i][j]);
 			j++;
 		}
-		printf("\n");
+		_putchar('\n');
 	}
 }
