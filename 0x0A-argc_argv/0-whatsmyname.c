2f925ac84e94e166de7b5ae71008614f36523d28
charifa <candely1charifa@gmail.com>
2023-04-10 09:10:07 -0700

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,11 @@
 
 /**
  * main - function a program that prints its name, followed by a new line
+ * @argc: variable
+ * @argv:variable
  * Return: result
  */
-int main (int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
