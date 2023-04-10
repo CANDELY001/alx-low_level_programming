e303cc1bf21a725b967b25a04a9507450827d8c7
charifa <candely1charifa@gmail.com>
2023-04-10 09:21:02 -0700

    task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that prints all arguments it receives
+ * @argc: variable
+ * @argv:variable
+ * Return: result
+ */
+int main(int argc, char *argv[])
+{
+	int c = 0;
+
+	for (; c < argc; c++)
+	{
+		printf("%s\n", argv[c]);
+	}
+	return (0);
+}
