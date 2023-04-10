a7fa36e02f60580933668ab78e0892fade2a8ec8
charifa <candely1charifa@gmail.com>
2023-04-10 09:29:58 -0700

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,10 +14,10 @@ int main(int argc __attribute__((unused)), char *argv[])
 	if (argc < 2)
 	{
 		printf("Error\n");
-		return (0);
+		return (1);
 	}
 
 	res = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", res);
-	return (1);
+	return (0);
 }
