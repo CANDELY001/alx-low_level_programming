f57b1a7f86cf412bb51f0faf05bcd95519084b5b
charifa <candely1charifa@gmail.com>
2023-04-11 15:16:08 -0700

    task0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include<stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * *create_array - unction that creates an array of chars, and initializes
+ * @size: parameter
+ * @c: parameter
+ * Return: resullt
+ */
+/* By CHARIFA MASBAHI*/
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(size * sizeof(char));
+
+	if (p == 0)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		*(p + 1) = c;
+		i++;
+	}
+	return (p);
+}
