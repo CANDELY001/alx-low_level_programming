f29b4552cda8309ede1da92a0e342a6045f83604
charifa <candely1charifa@gmail.com>
2023-04-11 16:44:04 -0700

    task3

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+#include<stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * free_grid - frees a 2 dimensional grid 
+ * @grid: parameter
+ * @height: parameter
+ * Return: void
+ */
+/* BY CHARIFA MASBAHI*/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height == 0)
+		return;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
