83918d1c45e8f22d3dc58ca49a9d605f15d0942e
charifa <candely1charifa@gmail.com>
2023-04-11 16:45:34 -0700

    task3

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 /**
- * free_grid - frees a 2 dimensional grid 
+ * free_grid - frees a 2 dimensional grid
  * @grid: parameter
  * @height: parameter
  * Return: void
