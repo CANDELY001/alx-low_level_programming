a418755acdffe6bc58475a2d2850cc1a981125a9
charifa <candely1charifa@gmail.com>
2023-04-11 17:12:05 -0700

    task5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -30,7 +30,7 @@ char *argstostr(int ac, char **av)
 		size++;
 		i++;
 	}
-	tab = (char *) malloc((size + 1) * sizeof(char));
+	t = (char *) malloc((size + 1) * sizeof(char));
 	if (t == NULL)
 		return (NULL);
 
