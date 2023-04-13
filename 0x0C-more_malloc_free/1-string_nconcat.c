29921797cebe33bd47a8b3e6d799dd120f08c502
charifa <candely1charifa@gmail.com>
2023-04-13 14:25:36 -0700

    laylahilalah

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,6 +30,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n  = strlen(s2);
 	}
 	p = (char *) malloc((strlen(s1) + n + 1) * sizeof(char));
+	if  (!p)
+	{
+		return (NULL);
+	}
 	while (i < strlen(s1))
 	{
 		*(p + i) = *(s1 + i);
