88d54432332ce032b103de830e9553d0d4f3806e
charifa <candely1charifa@gmail.com>
2023-04-17 16:35:33 -0700

    task finish

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,57 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stddef.h>
+char *_strdup(char *s);
+/**
+ * *new_dog - function that creates a new dog
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ * Return: result
+ */
+/* BY CHARIFA MASBAHI*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+
+	new = (dog_t *) malloc(sizeof(dog_t));
+
+	if (!new)
+	{
+		return (NULL);
+	}
+	(*new).name = _strdup(name);
+
+	if (!(*new).name)
+	{
+		free(new);
+		return (NULL);
+	}
+	(*new).age = age;
+	(*new).owner = _strdup(owner);
+
+	if (!(*new).owner)
+	{
+		free((*new).owner);
+		free(new);
+		return (NULL);
+	}
+	return (new);
+}
+/**
+ * *_strdup - function that duplicates a string
+ *  @s: paremeter
+ *  Return: result
+ */
+char *_strdup(char *s)
+{
+	size_t len = strlen(s) + 1;
+	char *copy = malloc(len);
+
+	if (copy)
+	{
+		return (NULL);
+	}
+	memcpy(copy, s, len);
+	return (copy);
+}
