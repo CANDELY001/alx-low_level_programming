6eea1ccf6e97e95a8d6147c4bb2bf1c2ebe3d58e
charifa <candely1charifa@gmail.com>
2023-04-17 15:07:09 -0700

    finish task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  function that will print the name of compiled file
+ * Return: result
+ */
+/* BY CHARIFA MASBAHI*/
+int main(void)
+{
+	printf("%d\n",__FILE__);
+	return (0);
+}
