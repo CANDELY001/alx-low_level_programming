8836cc3e2bcbd10bcf88b242e7f9d6160b4493b2
charifa <candely1charifa@gmail.com>
2023-04-17 15:35:35 -0700

    task finish

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * dog - struct of dog
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ */
+/* BY CHARIFA MASBAHI*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
