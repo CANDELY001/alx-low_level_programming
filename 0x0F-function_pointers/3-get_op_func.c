c7294d2f52641c29a80d62b1baff445c2078bcc1
charifa <candely1charifa@gmail.com>
2023-04-19 06:40:25 -0700

    finish task

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,7 +20,7 @@ int (*get_op_func(char *s))(int a, int b)
 
 	while (i < 6)
 	{
-		if (strcmp(ops[i].op, s) == 0 && ops[i].op != NULL)
+		if (strcmp(ops[i].op, s) == 0 || ops[i].op != NULL)
 		{
 			return (ops[i].f);
 		}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, res;
+	int num1, num2, res = 0;
 	char *c;
 
 	if (argc != 4)
@@ -19,9 +19,9 @@ int main(int argc, char *argv[])
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	c = (char *) argv[2];
+	c = argv[2];
 
-	if (get_op_func(c) == NULL)
+	if (get_op_func(c) == NULL || c[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
