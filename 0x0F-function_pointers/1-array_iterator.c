06cde39616d0507b469291a0d8368f798ae3046a
charifa <candely1charifa@gmail.com>
2023-04-19 03:57:01 -0700

    finish task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
- * array_iterator - funtion that executes a function given as a parameter on all ele in arr
+ * array_iterator - executes a function given as a parameter on all ele in arr
  * @array: parameter
  * @size: parameter
  * @action: pointer of funtion
@@ -11,7 +11,7 @@
 /* BY CHARIFA MASBAHI*/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i = 0;
+	unsigned long int i = 0;
 
 	while (i < size)
 	{
