1fd4f9323732d8ad2911266033d794a24b359d63
charifa <candely1charifa@gmail.com>
2023-04-20 04:07:52 -0700

    done

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i = 0;
 
-	if (*separator)
+	if (separator)
 	{
 		va_start(ap, n);
 		while (i < n)
