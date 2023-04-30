61135bb492f9de38a627d960693340cd95e1f82e
CANDELY001 <candely1charifa@gmail.com>
2023-04-30 13:56:08 -0700

    done 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,6 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (!(ls->next) || !ls)
 			return (-1);
 		ls = ls->next;
+		i++;
 	}
 	ls2 = ls->next;
 	ls->next = ls2->next;
