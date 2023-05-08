2eeb461a82a095369e7ccf18ffe3b42e6ad5193d
CANDELY001 <candely1charifa@gmail.com>
2023-05-08 02:48:34 -0700

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
-	fwr = write(f_op, text_content, i);
+	fwr = write(fop, text_content, i);
 	if (fwr < 0)
 		return (-1);
 	close(fop);
