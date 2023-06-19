a6105aefefb5f46f9950e575097dace2b3651431
CANDELY001 <candely1charifa@gmail.com>
2023-06-19 11:47:20 -0700

    GM magic

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/CANDELY001/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib.so 
+wget -P /tmp https://github.com/CANDELY001/alx-low_level_programming/blob/532272e2c713cf78e9e54d816e0e890353d3684e/0x18-dynamic_libraries/lib.so
 export LD_PRELOAD=/tmp/lib.so
